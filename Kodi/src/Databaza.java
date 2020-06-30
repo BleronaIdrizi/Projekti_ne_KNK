b80282339af7ceaffa33fcf53b7e049744a88a3e
rinesa muharremi <58374578+rinesam@users.noreply.github.com>
2020-06-30 20:21:28 +0200

    Update Databaza.java

--- Kodi/src/Databaza.java
+++ Kodi/src/Databaza.java
@@ -1,1 +1,26 @@
-kodi i db ne java
+package smtd;
+
+import java.sql.*;
+
+public class Databaza {
+	private static Connection dbConnection;
+	
+	private final static String host = "localhost";
+	private final static String dbName = "DBKNK";
+	private final static String username = "root";
+	private final static String password = "";
+	
+	public static Connection getConnection() {
+		if(dbConnection == null) {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				dbConnection = DriverManager.getConnection("jdbc:mysql://" + host+ "/" + dbName, username, password);
+				
+			} catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		return dbConnection;
+	}
+}

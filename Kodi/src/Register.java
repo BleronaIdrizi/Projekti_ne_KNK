6924da5f88f33d5c01926068010c65cd6e294d8d
BletaGashi <58531818+BletaGashi@users.noreply.github.com>
2020-06-30 20:24:50 +0200

    showAlert

--- Kodi/src/Register.java
+++ Kodi/src/Register.java
@@ -166,7 +166,14 @@ public class Register extends Application {
     }
 
   
-
+  private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
 
     public static void main(String[] args) {
         launch(args);

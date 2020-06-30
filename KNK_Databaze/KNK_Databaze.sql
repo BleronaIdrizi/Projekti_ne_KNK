6a9670522a16353ca044892ceb1f071172b4605f
Blinera <blinera.hysenaj@gmail.com>
2020-06-30 14:46:07 +0200

    Update KNK_Databaze.sql

--- KNK_Databaze/KNK_Databaze.sql
+++ KNK_Databaze/KNK_Databaze.sql
@@ -1,63 +1,69 @@
-DROP DATABASE IF EXISTS KNK_Databaze;
-CREATE DATABASE KNK_Databaze;
-USE KNK_Databaze;
+DROP DATABASE IF EXISTS DBKNK;
+CREATE DATABASE DBKNK;
+USE DBKNK;
 
-CREATE TABLE Student (
-Nr_ID VARCHAR(12),
-S_Emri VARCHAR(20) NOT NULL,
-S_Mbiemri VARCHAR(20) NOT NULL,
-Fakulteti VARCHAR(30) NOT NULL,
-Departamenti VARCHAR(30) NOT NULL,
-Datelindja VARCHAR(20) NOT NULL,
-Filloi_Studimet VARCHAR(20) NOT NULL,
-Mbaroi_Studimet VARCHAR(20)NOT NULL,
-Titulli VARCHAR(70) NOT NULL DEFAULT 'Bachelor Student',
-PRIMARY KEY (Nr_ID));
-
-CREATE TABLE Profesor (
-Profesori VARCHAR(20),
-Prof_Password VARCHAR(20) NOT NULL,
-PRIMARY KEY (Profesor));
+CREATE TABLE Studenti (
+Student_ID VARCHAR(20),
+Emri VARCHAR(50) NOT NULL,
+Mbiemri VARCHAR(50) NOT NULL,
+Fakulteti VARCHAR(50) NOT NULL,
+Departamenti VARCHAR(50) NOT NULL,
+VitiLindjes VARCHAR(50) NOT NULL,
+VendiLindjes VARCHAR(50) NOT NULL,
+MbaroiStudimet VARCHAR(50),
+VitiRegjistrimit VARCHAR(50) NOT NULL,
+Titulli VARCHAR(70) NOT NULL DEFAULT 'Student',
+PRIMARY KEY (Student_ID));
 
 CREATE TABLE Lendet (
-Lenda VARCHAR(30) NOT NULL,
+Lenda VARCHAR(50) NOT NULL,
 ECTS INT NOT NULL,
-Profesor VARCHAR(20) NOT NULL,
+Profesori VARCHAR(50) NOT NULL,
 PRIMARY KEY(Lenda));
 
+CREATE TABLE Profesori (
+Profesori VARCHAR(50),
+Prof_Password VARCHAR(50) NOT NULL,
+PRIMARY KEY (Profesori));
+
+
 CREATE TABLE Login (
-Nr_ID VARCHAR(12),
-Stud_Password VARCHAR(20) NOT NULL,
-FOREIGN KEY (Nr_ID) REFERENCES Student(Nr_ID),
-PRIMARY KEY(Nr_ID));
+Stud_ID VARCHAR(50),
+Stud_Password VARCHAR(50) NOT NULL,
+FOREIGN KEY (Student_ID) REFERENCES Studenti(Student_ID),
+PRIMARY KEY(Student_ID));
 
 CREATE TABLE Notat (
-Nr_ID VARCHAR(50) NOT NULL,
-Lenda VARCHAR(30) NOT NULL,
+Stud_ID VARCHAR(50) NOT NULL,
+Lenda VARCHAR(50) NOT NULL,
 ECTS INT NOT NULL,
-Profesor VARCHAR(20) NOT NULL,
+Profesori VARCHAR(50) NOT NULL,
 Nota INT NOT NULL,
-FOREIGN KEY (Nr_ID) REFERENCES Student(Nr_ID),
-FOREIGN KEY (Profesor) REFERENCES Profesor(Profesor));
+FOREIGN KEY (Student_ID) REFERENCES Studenti(Student_ID),
+FOREIGN KEY (Profesori) REFERENCES Profesori(Profesori));
 
-CREATE TABLE Vit (
-Viti VARCHAR(30) NOT NULL,
-Nr_ID VARCHAR(50) NOT NULL,
-Lenda VARCHAR(30) NOT NULL,
-ECTS INT NOT NULL,
-Profesor VARCHAR(20) NOT NULL,
-FOREIGN KEY (Nr_ID) REFERENCES Student(Nr_ID),
-FOREIGN KEY (Notat) REFERENCES Notat(Notat),
-FOREIGN KEY (Profesor) REFERENCES Profesor(Profesor));
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Matematika 1', '7', 'Valdete Hamiti');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Fizika 1', '6', 'Skender Ahmetaj');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Bazat e Elektroteknikes', '7', 'Luan Ahma');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Qarqe Digjitale', '5', 'Sabrije Osmanaj');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Anglisht', '5', 'Gazmend Pula');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Matematika2', '7', 'Valdete Hamiti');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Fizika 2', '6', 'Skender Ahmetaj');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Qarqe Elektrike', '7', 'Luan Ahma');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Algoritmet dhe Struktura e Te Dhenave', '5', 'Avni Rexhepi');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Gjuhe Programuese', '5', 'Avni Rexhepi');
+/*Viti dyte */
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Sinjale dhe Sisteme', '5', 'Vjosa Shatri');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Elektronike', '5', 'Qamil Kabashi');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Matematike 3K', '5', 'Qefsere Gjonbalaj');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Interneti', '5', 'Lule Ahmedi');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Baze e te dhenave', '5', 'Lule Ahmedi');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Programim i orientuar ne objekte', '5', 'Isak Shabani');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Siguri e te dhenave', '5', 'Blerim Rexha');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Rrjeta Kompjuterike', '5', 'Blerim Rexha');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Programimi ne Internet', '5', 'Valon Raca');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Arkitektura e Kompjutereve', '5', 'Valon Raca');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Komunikimi njeri-kompjuter', '5', 'Isak Shabani');
+INSERT INTO `dbknk`.`Lendet` (`Lenda`, `ECTS`, `Profesori`) VALUES ('Menaxhim i Projekteve', '5', 'Sevdije Alshiqi');
 
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Matematika 1', '8', 'Valdete Hamiti');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Bazat e Elektroteknikes', '8', 'Luan Ahma');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Fizika 1', '9', 'Skender Ahmetaj');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Gjuhe Programuese', '8', 'Avni Rexhepi');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Qarqe Digjitale', '7', 'Sabrije Osmanaj');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Anglisht', '9', 'Gazmend Pula');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Matematika 2', '7', 'Valdete Hamiti');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Fizika 2', '8', 'Skender Ahmetaj');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Qarqe Elektrike', '7', 'Luan Ahma');
-INSERT INTO `KNK_Databaze`.`Lendet` (`Viti`,`Lenda`, `ECTS`, `Profesor`) VALUES ('1','Algoritmet dhe Struktura e Te Dhenave', '9', 'Avni Rexhepi');
 
